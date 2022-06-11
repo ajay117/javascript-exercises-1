cf0b597ba3e429624218a3f232e2f4977883723b
Ajay KC <meajay64@gmail.com>
2022-06-11 23:34:31 +0900

    Code and test module

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,11 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(obj) {
+    let objCopy = [...obj];
+    let oldest = objCopy.sort(function(a,b) {
+            aAge = (a.yearOfDeath || new Date().getFullYear()) - a.yearOfBirth;
+            bAge = (b.yearOfDeath || new Date().getFullYear()) - b.yearOfBirth;
+        return bAge - aAge;
+    })
+    return oldest[0];
 };
 
 // Do not edit below this line
