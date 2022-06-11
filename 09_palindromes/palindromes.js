dc9d21ba54efd85517f0afd53dd20ca8c8b4e562
Ajay KC <meajay64@gmail.com>
2022-06-11 15:59:46 +0900

    Code and test module

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let plainStr = str.toLowerCase().split('')
+        .filter(elem => elem !== '!' && elem !== ' ' && elem !== ',' & elem !== '.')
+        .join('');
+    let reversedStr = plainStr.split('').reverse().join('');
+    if(plainStr === reversedStr) return true;
+    return false;
 };
 
 // Do not edit below this line
