0359644253b99b1b931259395ae7dea5b9b54807
Ajay KC <meajay64@gmail.com>
2022-06-11 17:49:50 +0900

    Refactor

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,21 +1,12 @@
 const fibonacci = function(num) {
-    // (1, 1, 2, 3)
-    //Initialize num1 and num2 variable
     if(num == 1 || num == 2) return 1;
     let num1 = 1,
         num2 = 1,
         newValue;
-        //If num === 2 return 1
-        //If num === 1 return 1 or,
     if(num < 0) return 'OOPS';
     for(let i = 3; i <= num; i++) {
-        //Inside a loop equal initialize index with 3, less than or 
-        //equal to num
-        //New value will be num1 + num2
         newValue = num1 + num2;
-        //Num1 will equal to num2
         num1 = num2;
-        //Num2 will equal to newValue
         num2 = newValue;
     }
     return newValue;
