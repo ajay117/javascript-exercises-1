d63c3931606544593c8fb42e3b1ae9d83e7e074b
Ajay KC <meajay64@gmail.com>
2022-06-11 13:35:19 +0900

    Code and test modules

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,30 @@
-const add = function() {
-	
+const add = function(num1,num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1,num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  let total = 0;
+  arr.forEach(num => total += num);
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let result = 1;
+  arr.forEach(num => result *= num);
+  return result;
 };
 
-const power = function() {
-	
+const power = function(num1,num2) {
+	return num1 ** num2;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if(num === 0 || num === 1) return 1;
+  return num * factorial(num - 1);
 };
 
 // Do not edit below this line
